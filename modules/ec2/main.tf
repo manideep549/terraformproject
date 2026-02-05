77b39d4cdbc1b5e612ba991e9be61679c27444b3
manideep549 <87817999+manideep549@users.noreply.github.com>
2026-02-05 21:03:39 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
-  key_name               = var.key_name
+##  key_name               = var.key_name
 
   user_data = <<-EOF
               #!/bin/bash
