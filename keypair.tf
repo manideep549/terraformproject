733e9d0d613ad138c52d1926c056620b6596acd5
manideep549 <manideepgodavarthi549@gmail.com>
2026-02-05 23:11:36 +0530

    changes

--- keypair.tf
+++ keypair.tf
@@ -4,6 +4,6 @@ resource "tls_private_key" "ec2" {
 }
 
 resource "aws_key_pair" "ec2" {
-  key_name   = "tf-generated-key"
+  key_name   = "tf-generated-key-v2"
   public_key = tls_private_key.ec2.public_key_openssh
 }
