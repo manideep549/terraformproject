07cb64a26b19c2f6597a336c08b67d008f891515
manideep549 <manideepgodavarthi549@gmail.com>
2026-02-05 13:08:47 +0530

    changes

---
+++ modules/vpc/main.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  tags = { Name = "tf-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.az
+  map_public_ip_on_launch = true
+  tags = { Name = "tf-public-subnet" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
